2289ea184238bbca0eb66242e98b9700c7f7d854
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-12-14 12:56:28 +0100

    Add a new raw sort function that runs over a given array

--- src/libluatimsort.c
+++ src/libluatimsort.c
@@ -14,19 +14,37 @@
 
 typedef struct userdata_s {
     lua_State *L;
-    lua_Integer table_absidx;
+    lua_Integer absidx;
 } userdata_t;
 
 int cmp(item_t v, item_t w, mergestate_t *ms)
 {
     userdata_t *ud = (userdata_t *) ms->userdata;
 
-    int table_absidx = ud->table_absidx;
+    int table_absidx = ud->absidx;
 
     lua_State *L = ud->L;
     lua_pushvalue(L, table_absidx + 2);
     lua_geti(L, table_absidx, v);
     lua_geti(L, table_absidx, w);
+
+    lua_call(L, 2, 1);
+
+    int lt = lua_toboolean(L, -1);
+
+    lua_pop(L, 1);
+
+    return lt;
+}
+
+int cmpraw(item_t v, item_t w, mergestate_t *ms)
+{
+    userdata_t *ud = (userdata_t *) ms->userdata;
+
+    lua_State *L = ud->L;
+    lua_pushvalue (L, ud->absidx);
+    lua_pushinteger (L, v);
+    lua_pushinteger (L, w);
 
     lua_call(L, 2, 1);
 
@@ -50,16 +68,20 @@ static int l_sort(lua_State *L) {
 
     userdata_t ud;
     ud.L = L;
-    ud.table_absidx = table_absidx;
+    ud.absidx = table_absidx;
 
-    lua_len(L, table_absidx);  // push on the stack the number of elements to sort.
+    lua_len(L, table_absidx);       // push on the stack the number of elements to sort.
     int nel = lua_tointeger(L, -1); // get that number.
     lua_pop(L, 1);                  // clean the stack.
 
     int reverse = lua_toboolean(L, -2);
 
+    sortslice_t perm = (sortslice_t) malloc (sizeof (item_t) * nel);
+
+    for (int i = 0; i < nel; i++) perm[i] = i + 1;  // the identity permutation according to Lua's one-based indexing.
+
     time_t starttime = time(NULL);
-    sortslice_t perm = timsort (nel, reverse, cmp, &ud);
+    perm = timsort (perm, nel, reverse, cmp, &ud);
     time_t endtime = time(NULL);
 
     if (perm == NULL) {
@@ -71,7 +93,7 @@ static int l_sort(lua_State *L) {
     lua_createtable(L, nel, 0);	// the sorting permutation
 
     for (int i = 0; i < nel; i++) {
-        int idx = perm[i];
+        item_t idx = perm[i];
 
         lua_geti(L, table_absidx, idx);
         lua_seti(L, -3, i + 1);
@@ -87,8 +109,34 @@ static int l_sort(lua_State *L) {
     return 3;
 }
 
+static int l_sortraw (lua_State *L) {
+
+    sortslice_t perm = (sortslice_t) lua_touserdata (L, -4);
+    int nel = lua_tointeger(L, -3);
+    int reverse = lua_toboolean(L, -2);
+    assert(lua_isfunction(L, -1));
+
+    userdata_t ud;
+    ud.L = L;
+    ud.absidx = lua_absindex (L, -1);
+
+    for (int i = 0; i < nel; i++) perm[i] = i + 1;  // the identity permutation according to Lua's one-based indexing.
+
+    perm = timsort (perm, nel, reverse, cmpraw, &ud);
+
+    if (perm == NULL) {
+        lua_pushnil (L);
+    } else {
+        lua_pushlightuserdata (L, perm);
+    }
+
+    return 1;
+}
+
+
 static const struct luaL_Reg timsort_reg [] = {
 	{"sort", l_sort},
+    {"sortraw", l_sortraw},
 	{NULL, NULL} /* sentinel */
 };
  
